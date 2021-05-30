f24a7120d55c33b51adf91d790aea18c8b6150b8
ErdemGKSL <74366943+ErdemGKSL@users.noreply.github.com>
2021-05-30 16:44:58 +0300

    Multiple Triggers Added

--- bot.js
+++ bot.js
@@ -33,70 +33,66 @@ client.on("ready", () => {
 client.on("message", (msg) => {
     const args = msg.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
-    let command =
-        client.commands.find(
-            (x) =>
-                (x.type == "command" || !x.type) &&
+    let commands = client.commands.filter(
+        (x) =>
+            ((x.type == "command" || !x.type) &&
                 ((x.aliases && x.aliases.includes(commandName)) ||
-                    x.trigger == commandName)
-        ) ||
-        client.commands.find(
-            (x) =>
-                (x.type == "contains" && msg.content.includes(x.trigger)) ||
-                (x.type == "start" && msg.content.startsWith(x.trigger)) ||
-                (x.type == "end" && msg.content.endsWith(x.trigger)) ||
-                (x.type == "exact" && msg.content == x.trigger) ||
-                (x.type == "regex" &&
-                    msg.content.match(new RegExp(x.trigger, "i")))
-        );
-
-    if (!command) return;
-    if (command.guildOnly && msg.channel.type == "dm")
-        return msg.lineReply("Bu komut sadece sunucularda çalışır!");
-    if (command.cooldown && command.cooldown.enable) {
-        let cnm = command.trigger + msg.author.id;
-        let type = command.cooldown.type;
-        let wait = 10000;
-        if (type == "member" && command.guildOnly) {
-            cnm = command.trigger + msg.author.id + msg.guild.id;
-        } else if (type == "any") {
-            cnm = command.trigger;
-        } else if (type == "guild" && command.guildOnly) {
-            cnm = command.trigger + msg.guild.id;
-        }
-        if (command.cooldown.timeout) {
-            wait = command.cooldown.timeout * 1000;
-        }
-        if (cooldown.has(cnm)) {
-            if (command.cooldown.errormsg) {
-                msg.lineReply(command.cooldown.errormsg);
-            } else {
-                msg.lineReply("Komut Bekleme Süresinde...");
+                    x.trigger == commandName)) ||
+            (x.type == "contains" && msg.content.includes(x.trigger)) ||
+            (x.type == "start" && msg.content.startsWith(x.trigger)) ||
+            (x.type == "end" && msg.content.endsWith(x.trigger)) ||
+            (x.type == "exact" && msg.content == x.trigger) ||
+            (x.type == "regex" && msg.content.match(new RegExp(x.trigger, "i")))
+    );
+    if (!commands) return;
+    commands.forEach((command) => {
+        if (command.guildOnly && msg.channel.type == "dm")
+            return msg.lineReply("Bu komut sadece sunucularda çalışır!");
+        if (command.cooldown && command.cooldown.enable) {
+            let cnm = command.trigger + msg.author.id;
+            let type = command.cooldown.type;
+            let wait = 10000;
+            if (type == "member" && command.guildOnly) {
+                cnm = command.trigger + msg.author.id + msg.guild.id;
+            } else if (type == "any") {
+                cnm = command.trigger;
+            } else if (type == "guild" && command.guildOnly) {
+                cnm = command.trigger + msg.guild.id;
             }
-            return;
-        } else {
-            cooldown.add(cnm);
-            setTimeout(() => {
-                cooldown.delete(cnm);
-            }, wait);
+            if (command.cooldown.timeout) {
+                wait = command.cooldown.timeout * 1000;
+            }
+            if (cooldown.has(cnm)) {
+                if (command.cooldown.errormsg) {
+                    msg.lineReply(command.cooldown.errormsg);
+                } else {
+                    msg.lineReply("Komut Bekleme Süresinde...");
+                }
+                return;
+            } else {
+                cooldown.add(cnm);
+                setTimeout(() => {
+                    cooldown.delete(cnm);
+                }, wait);
+            }
         }
-    }
-    if (
-        !(
-            msg.content.startsWith(prefix) ||
-            command.type == "contains" ||
-            "start" ||
-            "regex" ||
-            "end" ||
-            "exact"
+        if (
+            !(
+                msg.content.startsWith(prefix) ||
+                command.type == "contains" ||
+                "start" ||
+                "regex" ||
+                "end" ||
+                "exact"
+            )
         )
-    )
-        return;
-    try {
-        command.execute(msg, args, client, u);
-    } catch (e) {
-        console.error(e);
-        msg.lineReply("Bir hata oluştu!");
-    }
+            return;
+        try {
+            command.execute(msg, args, client);
+        } catch (e) {
+            console.error(e);
+            msg.lineReply("Bir hata oluştu!");
+        }
+    });
 });
 client.login(token);
