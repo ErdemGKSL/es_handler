c712903b492d4b1f3f2ff650a12c91856b8f87b3
Erdem <74366943+ErdemGKSL@users.noreply.github.com>
2021-06-01 19:36:50 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -9,7 +9,16 @@ module.exports = {
     //? Eğer Tetiklenme tipi "command" ise ana komut dışındaki tetiklenmeler.
     guildOnly: false,
     //? Sadece Sunucularda'mı Çalışsın
-    ignoreBots: true,
+    ignoreBots: true, //?Botlar umursanmasın mı
+    /**
+     * * 0: Herkes
+     * * 1: Alt Yetkililer (Mesajları yönet vb.)
+     * * 2: Orta Yetkililer (Kanalları veya Rolleri yönet vb.)
+     * * 3: Üst Yetkililer (BAN veya KICK)
+     * * 4: Yöneticiler (ADMINSTRATORS)
+     * * 5: Botun Sahipleri (config.json)
+     */
+    permLevel: 0,
     cooldown: {
         enable: true, //? true false
         timeout: 90, //? SANİYE | Seconds
